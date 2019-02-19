a471dba7d36ad01a2c6ca1e407eea357ebef9896
ncepuJelex <1170366657@qq.com>
2019-02-19 23:22:03 +0800

    scope

---
+++ src/main/java/com/jel/tech/config/MyConfig2.java
@@ -0,0 +1,26 @@
+package com.jel.tech.config;
+
+import com.jel.tech.model.Person;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.*;
+
+/**
+ * @author jelex.xu
+ * @create 2019-02-16 00:01
+ **/
+@Configuration
+@ComponentScan(value = "com.jel.tech")
+public class MyConfig2 {
+
+    /**
+     * 多实例时，ioc容器启动不会去创建对象，而单实例会！
+     * @return
+     */
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    @Bean
+    public Person person() {
+        System.out.println("person init");
+        return new Person("ls", 19);
+    }
+}
--- src/test/java/com/jel/tech/SpringTest.java
+++ src/test/java/com/jel/tech/SpringTest.java
@@ -1,6 +1,7 @@
 package com.jel.tech;
 
 import com.jel.tech.config.MyConfig;
+import com.jel.tech.config.MyConfig2;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,6 +13,15 @@ import java.util.Arrays;
  **/
 public class SpringTest {
 
+
+    @Test
+    public void fun2() {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig2.class);
+//        Object person = context.getBean("person");
+//        Object person2 = context.getBean("person");
+//        System.out.println(person == person2); // true
+    }
 
     @Test
     public void fun() {
