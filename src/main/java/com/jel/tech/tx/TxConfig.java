48486dfa4f37b54203a9da50af8e7bde8723b4f6
ncepuJelex <1170366657@qq.com>
2019-03-06 23:11:45 +0800

    tx测试成功！

--- src/main/java/com/jel/tech/tx/TxConfig.java
+++ src/main/java/com/jel/tech/tx/TxConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
@@ -14,6 +17,7 @@ import java.beans.PropertyVetoException;
  * @author jelex.xu
  * @create 2019-03-06 22:53
  **/
+@EnableTransactionManagement
 @ComponentScan("com.jel.tech.tx")
 @Configuration
 public class TxConfig {
@@ -36,5 +40,11 @@ public class TxConfig {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource());
 
         return jdbcTemplate;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws PropertyVetoException {
+
+        return new DataSourceTransactionManager(dataSource());
     }
 }
--- src/main/java/com/jel/tech/tx/UserService.java
+++ src/main/java/com/jel/tech/tx/UserService.java
@@ -2,6 +2,7 @@ package com.jel.tech.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author jelex.xu
@@ -13,6 +14,7 @@ public class UserService {
     @Autowired
     private UserDao userDao;
 
+    @Transactional
     public void add() {
 
         userDao.insert();
